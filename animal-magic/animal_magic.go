116bbe3a04ab0c2db99656abc04474b032115d12
shubham <shubhamverma1091@gmail.com>
2024-11-17 11:37:36 +0530

    animal-magic solution

--- animal-magic/animal_magic.go
+++ animal-magic/animal_magic.go
@@ -1,16 +1,44 @@
 package chance
 
+import (
+	"math/rand"
+)
+
+// animals holds the constant list of available animals
+var animals = []string{
+	"ant",
+	"beaver",
+	"cat",
+	"dog",
+	"elephant",
+	"fox",
+	"giraffe",
+	"hedgehog",
+}
+
 // RollADie returns a random int d with 1 <= d <= 20.
 func RollADie() int {
-	panic("Please implement the RollADie function")
+	const (
+		minDieValue = 1
+		maxDieValue = 20
+	)
+	return rand.Intn(maxDieValue) + minDieValue
 }
 
 // GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0.
 func GenerateWandEnergy() float64 {
-	panic("Please implement the GenerateWandEnergy function")
+	const maxEnergy = 12.0
+	return rand.Float64() * maxEnergy
 }
 
 // ShuffleAnimals returns a slice with all eight animal strings in random order.
 func ShuffleAnimals() []string {
-	panic("Please implement the ShuffleAnimals function")
+	// Create a copy to avoid modifying the original slice
+	result := make([]string, len(animals))
+	copy(result, animals)
+
+	rand.Shuffle(len(result), func(i, j int) {
+		result[i], result[j] = result[j], result[i]
+	})
+	return result
 }
