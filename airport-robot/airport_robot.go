724b86ce4310af427190ccdb536f25e72449ee8e
shubham <shubhamverma1091@gmail.com>
2024-11-17 12:13:26 +0530

    airport-robot solution

--- airport-robot/airport_robot.go
+++ airport-robot/airport_robot.go
@@ -1,5 +1,43 @@
 package airportrobot
 
-// Write your code here.
-// This exercise does not have tests for each individual task.
-// Try to solve all the tasks first before running the tests.
+import "fmt"
+
+// Greeter interface defines methods for language-specific greetings
+type Greeter interface {
+    LanguageName() string
+    Greet(visitorName string) string
+}
+
+// Italian implements the Greeter interface for Italian language
+type Italian struct{}
+
+// Portuguese implements the Greeter interface for Portuguese language
+type Portuguese struct{}
+
+// LanguageName returns the name of the Italian language
+func (i Italian) LanguageName() string {
+    return "Italian"
+}
+
+// Greet returns an Italian greeting for the given visitor name
+func (i Italian) Greet(visitorName string) string {
+    const italianGreeting = "Ciao %s!"
+    return fmt.Sprintf(italianGreeting, visitorName)
+}
+
+// LanguageName returns the name of the Portuguese language
+func (p Portuguese) LanguageName() string {
+    return "Portuguese"
+}
+
+// Greet returns a Portuguese greeting for the given visitor name
+func (p Portuguese) Greet(visitorName string) string {
+    const portugueseGreeting = "Olá %s!"
+    return fmt.Sprintf(portugueseGreeting, visitorName)
+}
+
+// SayHello creates a complete greeting message using the provided greeter
+func SayHello(visitorName string, greeter Greeter) string {
+    const greetingFormat = "I can speak %s: %s"
+    return fmt.Sprintf(greetingFormat, greeter.LanguageName(), greeter.Greet(visitorName))
+}
