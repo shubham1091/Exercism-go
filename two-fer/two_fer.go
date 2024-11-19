0a2d2d927ea8ee2dbd84d3cb876ff50de03eba2f
shubham <shubhamverma1091@gmail.com>
2024-11-19 20:17:42 +0530

    two_fer solution

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -1,15 +1,17 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package twofer should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package twofer provides a function to share cookies in a "two-fer" offer.
 package twofer
 
-// ShareWith should have a comment documenting it.
+import (
+	"fmt"
+	"strings"
+)
+
+// ShareWith returns a phrase indicating who gets the extra cookie.
+// If the name is empty, it defaults to "you".
 func ShareWith(name string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	name = strings.TrimSpace(name)
+	if name == "" {
+		return "One for you, one for me."
+	}
+	return fmt.Sprintf("One for %s, one for me.", name)
 }
