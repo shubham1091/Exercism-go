c9c794f65ed38fbf00b5ac1d3cc2436968b8ba06
shubham <shubhamverma1091@gmail.com>
2024-11-19 23:59:33 +0530

    leap solution

--- leap/leap.go
+++ leap/leap.go
@@ -1,15 +1,24 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package leap should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package leap
 
-// IsLeapYear should have a comment documenting it.
+// IsLeapYear determines if the given year is a leap year.
+//
+// A year is a leap year if:
+//   - It is divisible by 400, or
+//   - It is divisible by 4 but not divisible by 100.
+//
+// The function returns true if the year is a leap year, and false otherwise.
 func IsLeapYear(year int) bool {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	panic("Please implement the IsLeapYear function")
+	if year < 0 {
+		return false
+	}
+	switch {
+	case year%400 == 0:
+		return true
+	case year%100 == 0:
+		return false
+	case year%4 == 0:
+		return true
+	default:
+		return false
+	}
 }
