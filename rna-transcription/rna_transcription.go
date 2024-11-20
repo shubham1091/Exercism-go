3edb9b5bd31375eafc9d276e27423f9534d1161f
shubham <shubhamverma1091@gmail.com>
2024-11-20 22:06:47 +0530

    rna-transcription solution

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,5 +1,19 @@
 package strand
 
+import "strings"
+
+var dnaToRna = map[rune]rune{
+	'A': 'U',
+	'C': 'G',
+	'G': 'C',
+	'T': 'A',
+}
+
 func ToRNA(dna string) string {
-	panic("Please implement the ToRNA function")
+	var sb strings.Builder
+	sb.Grow(len(dna))
+	for _, nuc := range dna {
+		sb.WriteRune(dnaToRna[nuc])
+	}
+	return sb.String()
 }
