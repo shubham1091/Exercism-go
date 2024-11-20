d8830131087202264472987fe7a2f82b6119c99e
shubham <shubhamverma1091@gmail.com>
2024-11-20 17:39:49 +0530

    difference_of_squares solution

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -1,13 +1,21 @@
+// Package diffsquares is a small library for calculating values relating to squares of a number.
 package diffsquares
 
-func SquareOfSum(n int) int {
-	panic("Please implement the SquareOfSum function")
+// formulae found at
+// https://learnersbucket.com/examples/algorithms/difference-between-square-of-sum-of-numbers-and-sum-of-square-of-numbers/
+
+// SquareOfSum returns the square of the sum of numbers from 1 to the number passed in.
+func SquareOfSum(num int) int {
+	sum := (num * (num + 1)) / 2
+	return sum * sum
 }
 
-func SumOfSquares(n int) int {
-	panic("Please implement the SumOfSquares function")
+// SumOfSquares returns the sum of the squared numbers from 1 to the number passed in.
+func SumOfSquares(num int) int {
+	return (num * (num + 1) * ((num * 2) + 1)) / 6
 }
 
-func Difference(n int) int {
-	panic("Please implement the Difference function")
+// Difference returns the square of sums minus the sum of squares for the number passed.
+func Difference(num int) int {
+	return SquareOfSum(num) - SumOfSquares(num)
 }
