6ebbb3d0e5737ea8aa397bd59de284c09b3ade9d
shubham <shubhamverma1091@gmail.com>
2024-11-20 20:21:12 +0530

    resistor-color solution

--- resistor-color/resistor_color.go
+++ resistor-color/resistor_color.go
@@ -1,11 +1,37 @@
 package resistorcolor
 
-// Colors returns the list of all colors.
+
+var bands = map[string]int{
+	"black":  0,
+	"brown":  1,
+	"red":    2,
+	"orange": 3,
+	"yellow": 4,
+	"green":  5,
+	"blue":   6,
+	"violet": 7,
+	"grey":   8,
+	"white":  9,
+}
+
+var colorList []string
+
+func init() {
+	for color := range bands {
+		colorList = append(colorList, color)
+	}
+}
+
+// Colors returns the list of all colors sorted alphabetically.
 func Colors() []string {
-	panic("Please implement the Colors function")
+	return colorList
 }
 
 // ColorCode returns the resistance value of the given color.
+// Returns -1 if the color is not found to indicate an error.
 func ColorCode(color string) int {
-	panic("Please implement the ColorCode function")
+	if code, exists := bands[color]; exists {
+		return code
+	}
+	return -1 // Return -1 to indicate the color was not found
 }
