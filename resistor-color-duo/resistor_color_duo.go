c2d1077bdf1d6b8e9af33bd2df759181416079bb
shubham <shubhamverma1091@gmail.com>
2024-12-01 11:23:16 +0530

    resistor-color-duo solution

--- resistor-color-duo/resistor_color_duo.go
+++ resistor-color-duo/resistor_color_duo.go
@@ -1,6 +1,33 @@
 package resistorcolorduo
 
+var bands = map[string]int{
+	"black":  0,
+	"brown":  1,
+	"red":    2,
+	"orange": 3,
+	"yellow": 4,
+	"green":  5,
+	"blue":   6,
+	"violet": 7,
+	"grey":   8,
+	"white":  9,
+}
+
 // Value should return the resistance value of a resistor with a given colors.
 func Value(colors []string) int {
-	panic("Implement the Value function")
+	// Handle empty or insufficient colors
+    if len(colors) < 2 {
+        return 0 // or handle error according to requirements
+    }
+
+    // Calculate resistance using only first two colors
+    firstDigit, exists1 := bands[colors[0]]
+    secondDigit, exists2 := bands[colors[1]]
+
+    // Handle invalid colors
+    if !exists1 || !exists2 {
+        return 0 // or handle error according to requirements
+    }
+
+    return firstDigit*10 + secondDigit
 }
