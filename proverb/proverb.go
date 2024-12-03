66305eb155d89d0d972e3c875c0d238a620564b6
shubham <shubhamverma1091@gmail.com>
2024-12-03 16:59:29 +0530

    proverb solution

--- proverb/proverb.go
+++ proverb/proverb.go
@@ -1,15 +1,25 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package proverb should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package proverb provides functionality to generate a proverb based on a list of inputs.
 package proverb
 
-// Proverb should have a comment documenting it.
+import "fmt"
+
+// Proverb generates a proverb from a list of inputs.
+// It returns a slice of strings, each representing a line of the proverb.
 func Proverb(rhyme []string) []string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	panic("Please implement the Proverb function")
+	lineCount := len(rhyme)
+
+	result := make([]string, 0, lineCount)
+
+	if lineCount == 0 {
+		return result
+	}
+
+	for idx, word := range rhyme[1:] {
+		line := fmt.Sprintf("For want of a %s the %s was lost.", rhyme[idx], word)
+		result = append(result, line)
+	}
+	finalLine := fmt.Sprintf("And all for the want of a %s.", rhyme[0])
+	result = append(result, finalLine)
+
+	return result
 }
